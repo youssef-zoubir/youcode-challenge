651e027fb6f21d6d8606d51ab5791faeac7e7010
youssef <youssefzoubir999@gmail.com>
2023-09-13 15:38:52 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+   char firstName[20];
+   char lastName[20];
+   int age;
+   char sexe[20];
+   int number;
+
+
+   printf("\n");
+   printf("\n");
+
+   printf("Enter Your Firstname : ");
+   scanf("%s",&firstName);
+
+   printf("Enter Your Lastname : ");
+   scanf("%s",&lastName);
+
+   printf("Enter Your age : ");
+   scanf("%hd", &age);
+
+    printf("Enter Your Number : ");
+   scanf("%hd",&number);
+
+   printf("Enter Your sexe : ");
+   scanf("%s",&sexe);
+
+
+   printf("\n");
+
+   printf("Your Firstname is : %s\n", firstName);
+   printf("Your Lastname is : %s\n", lastName);
+   printf("Your age is : %hd\n", age);
+    printf("Your age is : %ld\n", number);
+   printf("Your sexe is : %s\n", sexe);
+
+
+
+
+
+
+   printf("\n");
+   printf("\n");
+   return 0;
+
+}
