688d04f1b69b2ad4101a1c9b90f94032f39f28ca
youssef <youssefzoubir999@gmail.com>
2023-09-13 21:00:51 +0200

    challenge two

---
+++ chalange4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main()
+{
+  int a;
+  int b;
+  int c;
+  int sum
+  printf("entrer valeur a: ");
+  scanf("%d",&a);
+
+  printf("entrer valeur b: ");
+  scanf("%d",&b);
+
+  printf("entrer valeur c: ");
+  scanf("%d",&c);
+
+  sum=a+b+c;
+
+  print("the sum is: %d\n",sum);
+    return 0;
+}
