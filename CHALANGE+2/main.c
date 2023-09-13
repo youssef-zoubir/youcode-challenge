ba362ec99347d345707b96d47c3b929d06d50b6b
youssef <youssefzoubir999@gmail.com>
2023-09-13 16:48:15 +0200

    CHALLENGE TWO

---
+++ CHALANGE+2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float  Fahrenheit,Celsius;
+  printf("entrer temperature in Fahrenheit:");
+  scanf("%f",&Fahrenheit);
+
+  Celsius = (Fahrenheit-32)/1.8;
+  printf("%.2f Fahrenheit = %.2f Celsius",Fahrenheit,Celsius);
+
+    return 0;
+}
