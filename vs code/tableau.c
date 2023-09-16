cf8279951e6188c2077d6f91f29e63d33d762ef9
youssef <youssefzoubir999@gmail.com>
2023-09-16 18:12:04 +0100

    ok

--- vs code/tableau.c
+++ vs code/tableau.c
@@ -1,11 +1,40 @@
 #include<stdio.h>
-int x , y , 
+
+int max();
 int main(){
    
-tab[n] 
-   printf("saisir la valeur de tableau");
+
+   printf("saisir la valeur de tableau: ");
    
-   
+   max();
 
   return 0;
+}
+
+int max(){
+  int  t[5]= {5,8,6,17,19};
+  int max = t[0];
+
+  for (int i = 0; i < 5; i++)
+  {
+if (max < t[i]) 
+{
+    max = t[i];
+}
+ }
+  printf("%d",max);
+}
+
+int min(){
+  int  t[5]= {5,8,6,17,19};
+  int min = t[0];
+
+  for (int i = 0; i < 5; i++)
+  {
+if (min < t[i]) 
+{
+    min = t[i];
+}
+ }
+  printf("%d",min);
 }
