0152b42db68a67c243ac3ae5022f65914aef723f
youssef <youssefzoubir999@gmail.com>
2023-09-18 11:53:06 +0100

    ok

--- vs code/les tableau/tab02.c
+++ vs code/les tableau/tab02.c
@@ -4,11 +4,34 @@ int main(){
    
 int z[10];
 int i;
+int min , max ;
+printf("entrer les 10 num : \n");
 for (i=0 ; i<= 10 ; i++){
 
-   scanf("%d",& z[i]);
+   printf("z[%d]= ",i );
+   scanf("%d",&z[i]);  
 
 }
+  min = z[0];
+  for(i=0;i<=10;i++){
+  
+  if (min >z[i])
+    min = z[i];
+  printf("le min num est : %d \n",min);
+     }
+
+   max =z[0];
+   for ( i = 0; i <= 10; i++)
+   {
+      if (max < z[i])
+        max = z[i];
+
+        printf("le max num est : %d", max);
+   }
+   
+
+return 0;
+
 
 
 
