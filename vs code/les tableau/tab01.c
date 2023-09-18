be8cde90d8b8a1a740653b282381694d322efdde
youssef <youssefzoubir999@gmail.com>
2023-09-18 11:04:16 +0100

    ok

---
+++ vs code/les tableau/tab01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+   
+   int y[]={1,2,3,4,5,6,7,8,9,10} , i;
+  for ( i = 1; i <= 10 ; i++)
+  {
+     printf("%d ,", i);
+  }
+  
+
+
+return 0;
+}
---
+++ vs code/les tableau/tab02.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+   
+int z[10];
+int i;
+for (i=0 ; i<= 10 ; i++){
+
+   scanf("%d",& z[i]);
+
+}
+
+
+
+}
